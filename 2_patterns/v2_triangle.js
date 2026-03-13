aee565fbd22315920f5189794bdb153952ea253e
vikash-sharma70 <vikashofficial9@gmail.com>
2026-03-13 21:30:58 +0530

    triangle pattern

---
+++ 2_patterns/v2_triangle.js
@@ -0,0 +1,54 @@
+//=====================P1=================//
+let n = 4
+
+for (let i = 0; i < n; i++){
+    let row = '';
+    for(let j = 0; j <= i; j++){
+        row += "*" + ' ';
+    }
+    console.log(row);
+    
+}
+
+// * 
+// * * 
+// * * * 
+// * * * * 
+
+
+//=====================P2=================//
+let m = 4;
+ 
+for(let i = 1; i<=m; i++){
+    let row = '';
+    for (j= 0; j< i; j++){
+        row += i + " ";
+    }
+    console.log(row);
+    
+}
+
+// 1 
+// 2 2 
+// 3 3 3 
+// 4 4 4 4
+
+
+//=====================P3=================//
+let p = 4;
+
+for(let i = 0; i<=p; i++){
+    let row = "";
+    let ch = String.fromCharCode(65 + i);
+    for (let j = 0; j<= i; j++){
+        row += ch + ' '
+    }
+    console.log(row);
+    
+}
+
+// A 
+// B B 
+// C C C 
+// D D D D 
+// E E E E E 
