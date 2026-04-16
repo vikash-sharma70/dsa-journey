70c6b9af4918bfc5bd760398a630f8a3f3564b1c
vikash-sharma70 <vikashofficial9@gmail.com>
2026-04-16 20:32:42 +0530

    find maximum in array using recursion

---
+++ Luv_babbar/L2_Recursion/v4_findMax.js
@@ -0,0 +1,54 @@
+//=====Using Math.min function=======//
+/*
+let arr = [20,4,2,0,34,21];
+
+let result = findMin(arr, 0);
+
+console.log("Min num is:", result);
+
+function findMin(arr, index){
+    //base case
+    if(index === arr.length - 1){
+        return arr[index]
+    }
+
+    //recursion
+    let recAns = findMin(arr, index+1);
+
+    return Math.min(arr[index], recAns);
+    
+}
+*/
+
+
+//========Without using Math.min=========//
+
+let arr = [20, 4, 2, 0, 23, 21];
+
+let result = findMax(arr, 0);
+console.log("Maximum is:", result);
+
+function findMax(arr, index) {
+
+    // Base Case
+    if (index === arr.length - 1) {
+        return arr[index];
+    }
+
+    // Recursion
+    let recAns = findMax(arr, index + 1);
+
+    // Compare current element with recursive answer
+    let currentValue = arr[index];   // current element
+    let recursiveValue = recAns;     // baaki array ka minimum
+
+    let result;
+
+    if (currentValue > recursiveValue) {
+        result = currentValue;
+    } else {
+        result = recursiveValue;
+    }
+
+    return result;
+}
