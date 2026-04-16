04213ed103dd5af72465747d972891090ebe7a6f
vikash-sharma70 <vikashofficial9@gmail.com>
2026-04-16 11:39:44 +0530

    Reverse an array

--- Luv_babbar/L2_Recursion/v1_printArray.js
+++ Luv_babbar/L2_Recursion/v1_printArray.js
@@ -1,16 +1,30 @@
-function printArray(arr, size, index) {
-    if(index >= size) {
-        return;
-    }
+// function printArray(arr, size, index) {
+//     if(index >= size) {
+//         return;
+//     }
 
-    console.log("Array is:", arr[index]);
-    printArray(arr, size, index+1);
+//     console.log("Array is:", arr[index]);
+//     printArray(arr, size, index+1);
     
 
-}
+// }
 
 let arr = [10, 20, 30, 40, 50];
 let size = 5;
 let index= 0;
 
-printArray(arr, size, index)
+// printArray(arr, size, index)
+reverseArr(arr, size, index)
+
+
+//============================================//
+
+function reverseArr(arr, size){
+    if(size == 0){
+        return;
+    }
+
+    console.log("Reverse Arr:", arr[size-1]);
+    reverseArr(arr, size-1);
+    
+}
