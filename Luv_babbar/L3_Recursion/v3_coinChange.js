42f1ca61ee8c724c23883ab43a648a784ede1d29
vikash-sharma70 <vikashofficial9@gmail.com>
2026-04-22 20:01:57 +0530

    coin change using recursion include/exclude

---
+++ Luv_babbar/L3_Recursion/v3_coinChange.js
@@ -0,0 +1,27 @@
+function coinChange(amount, coins) {
+
+    function solve(index, amount) {
+
+        //Base Case 1: exact amount bana liya
+        if (amount === 0) {
+            return 1;
+        }
+
+        //Base Case 2: amount negative or coins finished
+        if (amount < 0 || index === coins.length) {
+            return 0;
+        }
+
+        //Include current coin
+        let include = solve(index, amount - coins[index]);
+
+        //Exclude current coin
+        let exclude = solve(index + 1, amount);
+
+        return include + exclude;
+    }
+
+    return solve(0, amount);
+}
+
+console.log(coinChange(5, [1,2,5]));
