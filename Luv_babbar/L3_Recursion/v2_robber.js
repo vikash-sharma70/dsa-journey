04e47e32eb870a85bb41f5574bb470e38aa22379
vikash-sharma70 <vikashofficial9@gmail.com>
2026-04-22 10:24:09 +0530

    House robber

--- Luv_babbar/L3_Recursion/v2_robber.js
+++ Luv_babbar/L3_Recursion/v2_robber.js
@@ -1,19 +1,48 @@
+// function robber(nums, index){
+//     if(index >= nums.length) {
+//         return 0;
+//     }
+
+//     //include
+//     let includAns = nums[index] + robber(nums, index+2)
+
+//     //exclude
+//     let excludeAns = 0 + robber(nums, index+1);
+    
+//     let finalAns = Math.max(includAns, excludeAns);
+//     return finalAns;
+// }
+
+// let nums = [2,4,1,6,9,3,1]
+// let index = 0;
+// let ans=robber(nums, index);
+// console.log("Max amount is:", ans);
+
+
+//=============OR Without using Math.max=============//
 function robber(nums, index){
     if(index >= nums.length) {
         return 0;
     }
 
-    //include
-    let includAns = nums[index] + robber(nums, index+2)
+    // include
+    let includeAns = nums[index] + robber(nums, index + 2);
 
-    //exclude
-    let excludeAns = 0 + robber(nums, index+1);
-    
-    let finalAns = Math.max(includAns, excludeAns);
+    // exclude
+    let excludeAns = robber(nums, index + 1);
+
+    // manual max logic
+    let finalAns;
+
+    if(includeAns > excludeAns){
+        finalAns = includeAns;
+    } else {
+        finalAns = excludeAns;
+    }
+
     return finalAns;
 }
 
-let nums = [2,4,1,6,9,3,1]
-let index = 0;
-let ans=robber(nums, index);
+let nums = [2,4,1,6,9,3,1];
+let ans = robber(nums, 0);
 console.log("Max amount is:", ans);
