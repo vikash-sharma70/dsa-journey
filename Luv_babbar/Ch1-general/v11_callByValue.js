94ace76a234fa0375fba15349d6afa9c3d2192c7
vikash-sharma70 <vikashofficial9@gmail.com>
2026-04-25 20:46:35 +0530

    Call by value

--- Luv_babbar/Ch1-general/v11_callByValue.js
+++ Luv_babbar/Ch1-general/v11_callByValue.js
@@ -33,3 +33,66 @@ updateArr(nums);
 console.log(nums);
 // Output
 // [999, 20, 30]
+
+// Samajh
+// nums → address (say 1000)
+// arr → same address (1000)
+
+
+// IMPORTANT TRICK (INTERVIEW TRAP)
+function update(arr){
+    arr = [100,200];
+}
+
+let nums1 = [1,2,3];
+update(nums1);
+
+console.log(nums1);
+// 🧪 Output
+// [1,2,3]
+
+/*
+🧠 WHY??
+
+👉 Ye samajhna bahut important hai:
+arr = [100,200]
+
+👉 iska matlab:
+arr ab naye address ko point kar raha hai
+
+👉 but:
+nums → purane address pe hi hai */
+
+
+
+//==============DSA Use Case (🔥 REAL IMPACT)
+//✔️ Backtracking Example
+function solve(arr){
+    arr.push(100);
+    console.log("Inside:", arr);
+}
+
+let nums2 = [1,2];
+solve(nums2);
+console.log("Outside:", nums2);
+// Output
+// Inside: [1,2,100]
+// Outside: [1,2,100]
+
+// 👉 Isliye backtracking me:
+
+// push → pop karna zaroori hota hai
+// 🧠 5️⃣ How to avoid change (Clone)
+function safeUpdate(arr){
+    let copy = [...arr]; // clone
+    copy[0] = 999;
+    console.log("Inside:", copy);
+}
+
+let nums3 = [10,20];
+safeUpdate(nums3);
+
+console.log("Outside:", nums3);
+// Output
+// Inside: [999,20]
+// Outside: [10,20]
