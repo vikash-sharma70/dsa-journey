3fa7160c2d90c749b2d422d2ea12caf2205ec4fa
vikash-sharma70 <vikashofficial9@gmail.com>
2026-04-25 19:14:02 +0530

    Convert character in lower case

--- Luv_babbar/Ch1-general/v10_upperLowerCase.js
+++ Luv_babbar/Ch1-general/v10_upperLowerCase.js
@@ -24,3 +24,37 @@ Lower → Upper  = -32
 let char = 'A';
 let lower = char.toLowerCase();
 console.log("A in Lower case:", lower);
+
+
+//================2.ASCII / CharCode (DSA level)===========//
+function toLower(ch){
+    let ascii = ch.charCodeAt(0);
+
+    //check if upperCase
+    if(ascii >= 'A'.charCodeAt(0) && ascii <= 'Z'.charCodeAt(0) ){
+        let newAscii = ascii - 'A'.charCodeAt(0) + 'a'.charCodeAt(0);
+
+        return String.fromCharCode(newAscii);
+    }
+
+    return ch; //already lowercase
+}
+
+console.log("To Lower case:", toLower('M'));
+
+
+function toUpper(ch){
+    let ascii = ch.charCodeAt(0);
+
+    //check if upperCase
+    if(ascii >= 'a'.charCodeAt(0) && ascii <= 'z'.charCodeAt(0) ){
+        let newAscii = ascii - 'a'.charCodeAt(0) + 'A'.charCodeAt(0);
+
+        return String.fromCharCode(newAscii);
+    }
+
+    return ch; //already lowercase
+}
+
+console.log("To Upper case:", toUpper('m'));
+
