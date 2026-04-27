a1388e2657f4d5d84e2c3bf806c19010254b9d27
vikash-sharma70 <vikashofficial9@gmail.com>
2026-04-27 22:55:24 +0530

    Hallow aquare pattern

--- Luv_babbar/patterns/L2-Pattern_Printing/v4_halloSqr.js
+++ Luv_babbar/patterns/L2-Pattern_Printing/v4_halloSqr.js
@@ -1,21 +1,48 @@
 //Hallow square pattern
 hallowSqrPtr(5);
 
+// function hallowSqrPtr(n){
+//     for(let i = 1; i<=n; i++){
+
+//         let result = ""
+//         for(let j=1; j<=n; j++){
+
+//             if(i==1 || i==n){
+//                 result += "*"
+//             } else if(j==1 || j==n){
+//                 result += "*"
+//             } else {
+//                 result += " "
+//             }
+//         }
+//         console.log(result);
+        
+//     }
+// }
+
+//===============Optimized===========//
 function hallowSqrPtr(n){
-    for(let i = 1; i<=n; i++){
+    for(let i = 1; i <= n; i++){
 
-        let result = ""
-        for(let j=1; j<=n; j++){
+        let result = "";
 
-            if(i==1 || i==n){
-                result += "*"
-            } else if(j==1 || j==n){
-                result += "*"
+        for(let j = 1; j <= n; j++){
+
+            if(i === 1 || i === n || j === 1 || j === n){
+                result += "*";
             } else {
-                result += " "
+                result += " ";
             }
         }
+
         console.log(result);
-        
     }
 }
+
+/*
+👉 Always think:
+Top → i == 1
+Bottom → i == n
+Left → j == 1
+Right → j == n
+*/
