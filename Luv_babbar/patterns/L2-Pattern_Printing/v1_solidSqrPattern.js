74d64a46df61a46851642f1135c36d76579d6da3
vikash-sharma70 <vikashofficial9@gmail.com>
2026-04-27 19:29:41 +0530

    Added some comments

--- Luv_babbar/patterns/L2-Pattern_Printing/v1_solidSqrPattern.js
+++ Luv_babbar/patterns/L2-Pattern_Printing/v1_solidSqrPattern.js
@@ -1,8 +1,10 @@
 function solidSqr(num){
 
+    //Outer loop - no of rows
     for(let i = 1; i <= num; i++){
         let result = "";
         
+        //inner loop - no of columns, define the logic in inner loop
         for(let j = 1; j <= num; j++){
             if(j === num){
                 result += "*";   // Don't add space at last
