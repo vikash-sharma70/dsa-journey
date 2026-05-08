eac7dd6fa0dca9d33c09f7546fc04b71e03312a4
vikash-sharma70 <vikashofficial9@gmail.com>
2026-05-08 18:37:23 +0530

    Power of two number

--- Luv_babbar/arrays/L1_arrays/v7_isPowerOfTwo.js
+++ Luv_babbar/arrays/L1_arrays/v7_isPowerOfTwo.js
@@ -11,3 +11,15 @@ function isPowerOfTwo(n){
 
     return n===1 //true(like if n==1 then it's true)
 }
+
+//============OR using Bitwise operator is best approach===========//
+function isPowerOfTwo1(n){
+
+    if(n <= 0) return false;
+
+    return (n & (n - 1)) === 0;
+}
+
+// test
+console.log(isPowerOfTwo1(16)); // true
+console.log(isPowerOfTwo1(18)); // false
