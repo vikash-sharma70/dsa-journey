8c470aff2397cf83bd96d0b69ab57f2d6472f920
vikash-sharma70 <vikashofficial9@gmail.com>
2026-05-08 12:46:12 +0530

    reverse a num

--- Luv_babbar/arrays/L1_arrays/v5_revNum.js
+++ Luv_babbar/arrays/L1_arrays/v5_revNum.js
@@ -4,11 +4,31 @@ console.log(result);
 
 function reverseNum(num){
 
+    //Number ko string me convert kar diya ❌ so this approach is not good, use below one
     let rev = "";
 
     while(num!=0){
         let digit = num % 10;
         rev += digit
+        num = Math.floor(num/10)
+    }
+    return rev
+}
+
+
+//===========OR another approach============//
+
+let result1 = reverseNum1(857601)
+console.log(result1);
+
+
+function reverseNum1(num){
+
+    let rev = 0;
+
+    while(num!=0){
+        let digit = num % 10;
+        rev = rev*10 + digit
         num = Math.floor(num/10)
     }
     return rev
