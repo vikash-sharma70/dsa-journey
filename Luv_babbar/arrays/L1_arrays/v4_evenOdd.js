3b63bfe0fa530339749fc58df3210f677ef8e96f
vikash-sharma70 <vikashofficial9@gmail.com>
2026-05-08 12:18:51 +0530

    even or odd

--- Luv_babbar/arrays/L1_arrays/v4_evenOdd.js
+++ Luv_babbar/arrays/L1_arrays/v4_evenOdd.js
@@ -1,13 +1,21 @@
 // agar %(modulus) ko ignore kr skte he means agar program ko without using modulus bna skte ho to jarur banao kuki % operation bohot heavy op hote he to hum bitwise operator & ka use krke bhi is even or odd program ko bna skte he and bitwise operator se fast kuchh bhi nhi hota he.
 
 function checkEvOd(num){
-
+/*
     if(num%2==0){
         return "Even"
     } else{
         return "Odd"
     }
+        */
+
+    //======OR using bitwise op========
+    if(num & 1){
+        return "Odd"
+    } else{
+        return "Even"
+    }
 }
 
-let result = checkEvOd(450)
+let result = checkEvOd(451)
 console.log(result);
