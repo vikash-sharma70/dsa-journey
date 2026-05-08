dfb7050f061281f58fa21454c69d25d7659a5429
vikash-sharma70 <vikashofficial9@gmail.com>
2026-05-08 12:58:11 +0530

    reverse a num

--- Luv_babbar/arrays/L1_arrays/v5_revNum.js
+++ Luv_babbar/arrays/L1_arrays/v5_revNum.js
@@ -33,3 +33,33 @@ function reverseNum1(num){
     }
     return rev
 }
+
+
+//===========Rev -ive or +iv num===========
+function reverseInteger(num){
+
+    // Step 1: store the sign
+    let sign = num < 0 ? -1 : 1;
+
+    // Step 2: Make number positive
+    num = Math.abs(num);
+
+    let rev = 0;
+
+    // Step 3: reverse logic
+    while(num != 0){
+
+        let digit = num % 10;
+
+        rev = rev * 10 + digit;
+
+        num = Math.floor(num / 10);
+    }
+
+    // Step 4: apply sign again
+    return rev * sign;
+}
+
+
+console.log(reverseInteger(748372));   // 273847
+console.log(reverseInteger(-748372));  // -273847
