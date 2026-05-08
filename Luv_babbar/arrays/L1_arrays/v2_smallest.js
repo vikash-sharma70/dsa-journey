57df96b9be3d09e5d82b4b9b0c6533856ca48700
vikash-sharma70 <vikashofficial9@gmail.com>
2026-05-08 11:45:14 +0530

    Smallest digit

---
+++ Luv_babbar/arrays/L1_arrays/v2_smallest.js
@@ -0,0 +1,22 @@
+// ex:598153076 so in this number 0 is the smallest
+
+let smallestNumIs = smallestNum(598153076)
+console.log(smallestNumIs);
+
+
+function smallestNum(n){
+
+    let smallest=Infinity;
+
+    while(n!=0){
+
+        let digit = n%10;
+        if(digit<smallest){
+            smallest = digit
+        }
+        n = Math.floor(n/10)
+    }
+
+    return smallest
+
+}
