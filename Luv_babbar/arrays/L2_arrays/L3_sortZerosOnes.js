e961ff3614b46c1c3a700870e6740331cc898492
vikash-sharma70 <vikashofficial9@gmail.com>
2026-05-09 19:19:42 +0530

    sort zeros and ones

--- Luv_babbar/arrays/L2_arrays/L3_sortZerosOnes.js
+++
@@ -1,8 +0,0 @@
-// ✅ 2️⃣ Count Method (Better 👍)
-let result = sortZerosOnes([1,0,1,0,1,0,1])
-console.log(result);
-
-function sortZerosOnes(arr){
-
-    
-}
---
+++ Luv_babbar/arrays/L2_arrays/v4_sortZerosOnes.js
@@ -0,0 +1,56 @@
+//2️⃣ Count Method (Better)
+let result = sortZerosOnes([1,0,1,0,1,0,1])
+console.log(result);
+
+function sortZerosOnes(arr){
+    let countZeros=0
+
+    let newArr = arr;
+
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]==0){
+            countZeros++;
+        }
+    }
+
+    for(let i=0; i<countZeros; i++){
+        newArr[i] = 0;
+    }
+
+    for(let i=countZeros; i<arr.length; i++){
+        newArr[i] =1;
+    }
+
+    return newArr
+
+}
+
+
+//============ 3️⃣ Best Approach (Two Pointer)
+let result1 = sortZeroOne([1,0,1,0,1,0,1])
+console.log("Two pointer:",result1);
+
+function sortZeroOne(arr){
+
+    let left = 0;
+    let right = arr.length-1;
+    let newArr = arr;
+
+    while(left<right){
+        if(newArr[left] === 0){
+            left++
+        }
+        else if(newArr[right] ===1){
+            right--;
+        }
+        else{
+            let temp = newArr[left];
+            newArr[left]=newArr[right];
+            newArr[right] = temp;
+        }
+
+    }
+
+    return newArr;
+}
+
