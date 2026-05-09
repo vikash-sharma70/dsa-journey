5845c3c0c542437807e97b8ad22ea6cf2f4e4016
vikash-sharma70 <vikashofficial9@gmail.com>
2026-05-09 19:50:40 +0530

    array elements triplates

--- Luv_babbar/arrays/L2_arrays/v5_pairs.js
+++ Luv_babbar/arrays/L2_arrays/v5_pairs.js
@@ -1,14 +1,26 @@
-let arr = [10, 20, 30, 40];
+let arr = [10, 20, 30, 40, 50, 60, 70];
 
 printPairs(arr);
 
 function printPairs(arr){
 
     for(let i = 0; i < arr.length; i++){
-
-        for(let j = 0; j < arr.length; j++){
-
+        for(let j = i + 1; j < arr.length; j++){
             console.log(`(${arr[i]}, ${arr[j]})`);
+        }
+    }
+}
+
+//===========print triplates==========//
+printPairs1(arr);
+
+function printPairs1(arr){
+
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i + 1; j < arr.length; j++){
+            for(let k = j + 1; k < arr.length; k++){
+            console.log("Triplates:",`(${arr[i]}, ${arr[j]}, ${arr[k]})`);
+            }
         }
     }
 }
