9d3a618e7961cecd889c8dcc4ba3309767a7530a
vikash-sharma70 <vikashofficial9@gmail.com>
2026-05-09 11:53:46 +0530

    Second largest

---
+++ Luv_babbar/arrays/L2_arrays/v1_secondLargest.js
@@ -0,0 +1,27 @@
+let result = secondLarg([34,11,0,45,12,38,1])
+console.log(result);
+
+function secondLarg(arr){
+    let largest = -Infinity;
+    let secondLarg = -Infinity;
+
+    for(let i = 0; i<arr.length; i++){
+
+       // Key Insight (Golden Logic)
+         // New largest milta hai → purana largest → secondLargest ban jata hai
+        if(arr[i]> largest){
+            secondLarg = largest;
+            largest = arr[i]
+        } else if(arr[i] > secondLarg && arr[i] !== largest){
+            secondLarg = arr[i]
+        }
+    }
+
+    return {
+        large: largest,
+        secondLarge: secondLarg,
+    }
+
+}
+
+
