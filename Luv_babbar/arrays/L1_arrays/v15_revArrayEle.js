63d664cfd21282cb8f2da0a9c5c1bad2bb598a00
vikash-sharma70 <vikashofficial9@gmail.com>
2026-05-09 18:25:23 +0530

    Reverse an array

--- Luv_babbar/arrays/L1_arrays/v15_revArrayEle.js
+++ Luv_babbar/arrays/L1_arrays/v15_revArrayEle.js
@@ -12,3 +12,27 @@ let arr1 = [10, 20, 30, 40];
 arr1.reverse();
 
 console.log(arr1);
+
+
+
+//===============Best Approach (Two Pointer)===========//
+
+let arr2 = [22,12,0,43,9]
+revArrayTwoPointer(arr2)
+console.log(arr2);
+
+function revArrayTwoPointer(arr2){
+
+    let left = 0 //from 0 index
+    let right = arr2.length-1
+
+    while(left<=right){
+        let temp = arr2[left];
+        arr2[left] = arr2[right];
+        arr2[right] = temp;
+        left++;
+        right--;
+    }
+
+
+}
