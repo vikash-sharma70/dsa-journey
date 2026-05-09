300b5083b5ecf387fcbd0ef540a03949bf76cdc3
vikash-sharma70 <vikashofficial9@gmail.com>
2026-05-09 19:42:34 +0530

    array elements pairs

---
+++ Luv_babbar/arrays/L2_arrays/v5_pairs.js
@@ -0,0 +1,14 @@
+let arr = [10, 20, 30, 40];
+
+printPairs(arr);
+
+function printPairs(arr){
+
+    for(let i = 0; i < arr.length; i++){
+
+        for(let j = 0; j < arr.length; j++){
+
+            console.log(`(${arr[i]}, ${arr[j]})`);
+        }
+    }
+}
